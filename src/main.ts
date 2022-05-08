67b0dca807244c753f609b74e96a5d7b297a3fd4
NotTimIsReal <alastairrintumathew@gmail.com>
2022-05-08 19:36:04 +1000

    .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-
+import { config } from 'dotenv';
 async function bootstrap() {
+  config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  app.set('trust proxy', 1);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
