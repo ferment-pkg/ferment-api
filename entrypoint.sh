c99f03092ce4cbeda9a07047e0e5deca23a24810
NotTimIsReal <alastairrintumathew@gmail.com>
2022-07-30 21:18:57 +1000

    entrypoint change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
-git stash
 pull=$(git pull)
-git stash drop 0
 if [[ "$pull" == *"Already up to date."* ]]; then
   yarn start:prod
   exit 0
+fi
+if [[ "$pull" == *"error: Your local changes to the following files would be overwritten by merge:"* ]]; then
+  git fetch
+  git reset --hard origin/main
 fi
 yarn install
 yarn build
